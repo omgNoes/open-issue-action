95054668c3e1af5e5549dd2b41a83e82ffaf5c3c
Ramses Rodenburg <ramses@info.nl>
2024-05-15 00:00:04 +0200

    debug #1

--- src/main.js
+++ src/main.js
@@ -21,9 +21,9 @@ async function run() {
     const octokit = github.getOctokit(token)
 
     const response = await octokit.rest.issues.create({
-      // owner: github.context.repo.owner,
-      // repo: github.context.repo.repo,
-      ...github.context.repo,
+      owner: github.context.repo.owner,
+      repo: github.context.repo.repo,
+      // ...github.context.repo,
       title,
       body,
       assignee
